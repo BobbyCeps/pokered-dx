ffa3f391c4469dbf289aa1f8d6f8a7a01134bc75
Utena <VincentSimonlille@gmail.com>
2024-10-09 21:02:02 +0200

    No Badges stats boost / boulderdust fix

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -6652,7 +6652,7 @@ ApplyBadgeStatBoosts:
 	ld a, [wLinkState]
 	cp LINK_STATE_BATTLING
 	ret z ; return if link battle
-	ld a, [wObtainedBadges]
+	ld a, 0 ;was [wObtainedBadges]
 	ld b, a
 	ld hl, wBattleMonAttack
 	ld c, $4
