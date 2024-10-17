d1d0fcc42a0686271a507553f67f55ab63ac7f16
Utena <VincentSimonlille@gmail.com>
2024-10-17 23:59:37 +0200

    Revert "rematch brock"
    This reverts commit 37e86964dd4c2ae8c09dbc08722a4eef83f5c6ed.

--- scripts/PewterGym.asm
+++ scripts/PewterGym.asm
@@ -42,10 +42,6 @@ PewterGymBrockPostBattle:
 	jp z, PewterGymResetScripts
 	ld a, D_RIGHT | D_LEFT | D_UP | D_DOWN
 	ld [wJoyIgnore], a
-	ld a, [wBeatGymFlags] ;rematch
-	bit BIT_EARTHBADGE, a
-	and a
-	jr nz, BrockRematchPostBattle
 ; fallthrough
 PewterGymScriptReceiveTM34:
 	ld a, TEXT_PEWTERGYM_BROCK_WAIT_TAKE_THIS
@@ -92,19 +88,12 @@ PewterGym_TextPointers:
 	dw_const PewterGymBrockWaitTakeThisText, TEXT_PEWTERGYM_BROCK_WAIT_TAKE_THIS
 	dw_const PewterGymReceivedTM34Text,      TEXT_PEWTERGYM_RECEIVED_TM34
 	dw_const PewterGymTM34NoRoomText,        TEXT_PEWTERGYM_TM34_NO_ROOM
-	dw_const PewterGymRematchPostBattleText, TEXT_PEWTERGYM_REMATCH_POST_BATTLE
 
 PewterGymTrainerHeaders:
 	def_trainers 2
 PewterGymTrainerHeader0:
 	trainer EVENT_BEAT_PEWTER_GYM_TRAINER_0, 5, PewterGymCooltrainerMBattleText, PewterGymCooltrainerMEndBattleText, PewterGymCooltrainerMAfterBattleText
 	db -1 ; end
-	
-BrockRematchPostBattle: ;rematch
-	ld a, TEXT_PEWTERGYM_REMATCH_POST_BATTLE
-	ldh [hSpriteIndexOrTextID], a
-	call DisplayTextID
-	jp PewterGymResetScripts
 
 PewterGymBrockText:
 	text_asm
@@ -116,10 +105,6 @@ PewterGymBrockText:
 	call DisableWaitingAfterTextDisplay
 	jr .done
 .afterBeat
-	ld a, [wBeatGymFlags] ;rematch
-	bit BIT_EARTHBADGE, a
-	and a
-	jr nz, .BrockRematch
 	ld hl, .PostBattleAdviceText
 	call PrintText
 	jr .done
@@ -140,30 +125,6 @@ PewterGymBrockText:
 	ld [wGymLeaderNo], a
 	xor a
 	ldh [hJoyHeld], a
-	jr .endBattle ;rematch
-.BrockRematch
-	ld hl, .PreBattleRematch1Text
-	call PrintText
-	call YesNoChoice
-	ld a, [wCurrentMenuItem]
-	and a
-	jr nz, .refused
-	ld hl, .PreBattleRematch2Text
-	call PrintText
-	call Delay3
-	ld a, OPP_BROCK
-	ld [wCurOpponent], a
-	ld a, 2
-	ld [wTrainerNo], a
-	ld a, $4 ; new script
-	ld [wPewterGymCurScript], a
-	ld [wCurMapScript], a
-	jr .endBattle
-.refused
-	ld hl, .PreBattleRematchRefusedText
-	call PrintText
-	jr .done
-.endBattle	
 	ld a, SCRIPT_PEWTERGYM_BROCK_POST_BATTLE
 	ld [wPewterGymCurScript], a
 	ld [wCurMapScript], a
@@ -176,22 +137,6 @@ PewterGymBrockText:
 
 .PostBattleAdviceText:
 	text_far _PewterGymBrockPostBattleAdviceText
-	text_end
-
-.PreBattleRematch1Text:
-		text_far _PewterGymRematchPreBattle1Text
-		text_end
-
-.PreBattleRematchRefusedText:
-		text_far _GymRematchRefusedText
-		text_end
-
-.PreBattleRematch2Text:
-		text_far _PewterGymPreRematchBattle2Text
-		text_end
-
-PewterGymRematchPostBattleText:
-	text_far _PewterGymRematchPostBattleText
 	text_end
 
 PewterGymBrockWaitTakeThisText:
