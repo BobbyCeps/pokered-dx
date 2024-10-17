e0eb99af2b25cdddc3d96ad7163547645e0d698e
Utena <VincentSimonlille@gmail.com>
2024-10-18 00:48:03 +0200

    Reapply "Morgane rematch"
    This reverts commit f325847928b440c0ef9bdaf2c2ae427938549d49.

--- text/SaffronGym.asm
+++ text/SaffronGym.asm
@@ -44,6 +44,40 @@ _SaffronGymSabrinaPostBattleAdviceText::
 	cont "dons!"
 	done
 
+_SaffronGymRematchPreBattle1Text:: ;rematch
+	text "SABRI our last"
+	line "battle, my"
+	para "#MON have"
+	line "grown!"
+	para "ROCK-hard will"
+	line "meets peak skill"
+	cont "today!"
+	para "Ready for the"
+	line "rematch of a"
+	cont "lifetime?"
+	done
+;_GymRematchRefusedText::
+;	text "Maybe some other"
+;	line "time."
+;	done
+_SaffronGymPreRematchBattle2Text::
+	text "SABRIsharpen!"
+	line "Let's battle!"
+	done
+_SaffronGymRematchPostBattleText::
+	text "SABRINA"
+	para "Your strength"
+	line "is unmatched!"
+	para "True to your"
+	line "title, CHAMPION."
+	para "Continue to lead"
+  line "#MON with"
+  cont "honor and"
+	cont "courage!"
+	para "New challenges"
+	line "await."
+	done ;rematch
+	
 _SaffronGymSabrinaMarshBadgeInfoText::
 	text "Avec le BADGE"
 	line "MARAIS, les"
