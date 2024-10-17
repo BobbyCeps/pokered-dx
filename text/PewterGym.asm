2b8fa3018bca04717730b8b068c2969bc4bb12b2
Utena <VincentSimonlille@gmail.com>
2024-10-18 00:47:49 +0200

    Reapply "rematch brock"
    This reverts commit d1d0fcc42a0686271a507553f67f55ab63ac7f16.

--- text/PewterGym.asm
+++ text/PewterGym.asm
@@ -18,3 +18,47 @@ _PewterGymBrockPreBattleText::
 	cont "moi? Très bien."
 	cont "En garde!"
 	done
+
+_PewterGymRematchPreBattle1Text::
+	text "Since our last"
+	line "battle, my"
+
+	para "#MON have"
+	line "grown!"
+
+	para "ROCK-hard will"
+	line "meets peak skill"
+	cont "today!"
+
+	para "Ready for the"
+	line "rematch of a"
+	cont "lifetime?"
+	done
+
+_GymRematchRefusedText::
+	text "Maybe some other"
+	line "time."
+	done
+
+_PewterGymPreRematchBattle2Text::
+	text "Stones sharpen!"
+	line "Let's battle!"
+	done
+
+_PewterGymRematchPostBattleText::
+	text "Incredible!"
+
+	para "Your strength"
+	line "is unmatched!"
+
+	para "True to your"
+	line "title, CHAMPION."
+
+	para "Continue to lead"
+  line "#MON with"
+  cont "honor and"
+	cont "courage!"
+
+	para "New challenges"
+	line "await."
+	done
